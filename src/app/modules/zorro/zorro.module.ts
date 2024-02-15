19bc4cdf15b93192b0d96f354f5a3ef296e3e6a1
nudopnu <forsuse@live.de>
2024-02-15 14:21:46 +0100

    Minor changes

--- src/app/modules/zorro/zorro.module.ts
+++ src/app/modules/zorro/zorro.module.ts
@@ -4,12 +4,13 @@ import { NzListModule } from 'ng-zorro-antd/list';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { IconDefinition } from '@ant-design/icons-angular';
-import { UserOutline, DownOutline } from '@ant-design/icons-angular/icons';
+import { UserOutline, DownOutline, InboxOutline } from '@ant-design/icons-angular/icons';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 
 const icons: IconDefinition[] = [
   UserOutline,
   DownOutline,
+  InboxOutline,
 ];
 
 @NgModule({
