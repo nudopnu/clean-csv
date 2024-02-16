e1fba4180870d271e781820cb2468a1de5cbfa20
nudopnu <forsuse@live.de>
2024-02-16 22:32:14 +0100

    Started basic layouting

--- src/app/modules/zorro/zorro.module.ts
+++ src/app/modules/zorro/zorro.module.ts
@@ -4,16 +4,24 @@ import { NzListModule } from 'ng-zorro-antd/list';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { IconDefinition } from '@ant-design/icons-angular';
-import { UserOutline, DownOutline, InboxOutline } from '@ant-design/icons-angular/icons';
+import { UserOutline, DownOutline, InboxOutline, SettingOutline, BgColorsOutline, MenuFoldOutline, MenuUnfoldOutline, QuestionOutline } from '@ant-design/icons-angular/icons';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzTabsModule } from 'ng-zorro-antd/tabs';
 import { NzTableModule } from 'ng-zorro-antd/table';
-
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
 
 const icons: IconDefinition[] = [
   UserOutline,
   DownOutline,
   InboxOutline,
+  SettingOutline,
+  BgColorsOutline,
+  MenuFoldOutline,
+  MenuUnfoldOutline,
+  QuestionOutline,
 ];
 
 @NgModule({
@@ -28,6 +36,10 @@ const icons: IconDefinition[] = [
     NzButtonModule,
     NzTabsModule,
     NzTableModule,
+    NzLayoutModule,
+    NzBreadCrumbModule,
+    NzDividerModule,
+    NzDrawerModule,
   ]
 })
 export class ZorroModule { }
